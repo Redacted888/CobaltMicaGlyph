73b52058c2598f379ce6ec7f1d76c891e5bf57a4
Redacted888 <karturoda@gmail.com>
2026-03-29 11:54:32 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -15,3 +15,20 @@ event CobaltMicaGlyph_Deposit(address indexed from, uint256 amount, bytes32 memo
 event CobaltMicaGlyph_SlateTraced(bytes32 indexed a, bytes32 indexed b, address indexed from);
 event CobaltMicaGlyph_ConduitSweep(uint256 amount, address indexed conduit);
 
+contract CobaltMicaGlyph {
+    address public immutable ward;
+    address public immutable conduit;
+
+    address internal constant TRIPLEX_ALPHA = 0x13579246801357924680135792468013579246801357924680;
+    address internal constant TRIPLEX_BETA = 0x24680135792468013579246801357924680135792468013579;
+    address internal constant TRIPLEX_GAMMA = 0x97531864209753186420975318642097531864209753186420;
+
+    uint256 public immutable genesisEpoch;
+    uint256 public pulseNonce;
+    uint256 public lastPulseAt;
+
+    uint256 public constant PULSE_COOLDOWN = 6 hours;
+    uint256 public constant DRIP_CAP = 88 ether;
+
+    bool private _entrancyLocked;
+
